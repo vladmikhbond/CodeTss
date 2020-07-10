2f519264f01d28319d8e5b6c0c3707219aae315c
volodymyr bondariev <vladmikhbond@hotmail.com>
2020-07-10 22:32:40 +0300

    .

--- src/web.js
+++ src/web.js
@@ -1,4 +1,4 @@
-const vscode = require('vscode');
+// const vscode = require('vscode');
 const querystring = require('querystring');
 const http = require('http');
 
@@ -9,141 +9,105 @@ const PORT = 49847;
 
 let TOKEN;
 
-//
-function token(name, pass, callback) 
+// 
+function token(name, pass) 
 {
-  var postData = querystring.stringify({
-    'username' : name, 
-    'password': pass       
-  });
+    return new Promise(function(resolve, reject) {
 
-  var options = {
-    hostname: HOST,
-    port: PORT,
-    path: '/api/token',
-    method: 'POST',
-    headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': postData.length
-    }
-  };
-    
-  var req = http.request(options, (res) => {
-    if (res.statusCode > 299)
-        vscode.window.showWarningMessage('Wrong login or pass.');
+        var postData = querystring.stringify({
+          'username' : name, 
+          'password': pass       
+        });
 
-    let data = "";
-    
-    res.on('data', (d) => {
-      data += d.toString();
-    });
-    
-    res.on('end', () => {
-      let obj = JSON.parse(data);
-      TOKEN = obj["access_token"];
-      vscode.window.showInformationMessage('Successful login');
-      if (callback) 
-          callback(); 
-    });
+        var options = {
+          hostname: HOST,
+          port: PORT,
+          path: '/api/token',
+          method: 'POST',
+          headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': postData.length
+          }
+        };
+          
+        var req = http.request(options, (res) => {
+          if (res.statusCode > 299)
+              reject('Wrong login or pass.');
 
-  });
-  
-  req.on('error', (e) => {
-    console.error(e);
-  });
-  
-  req.write(postData);
-  req.end();
-}
-
-
-function tss_task(taskId, callback) 
-{
-    var postData = querystring.stringify({
-      'taskId' : taskId
-    });       
-
-    var options = {
-      hostname: HOST,
-      port: PORT,
-      path: '/examine/tsstask',
-      method: 'POST',
-      headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': postData.length,
-            'Authorization': "Bearer " + TOKEN
-      }
-    };
-
-    
-    var req = http.request(options, (res) => {
-
-        // vscode.window.showInformationMessage('tss_task: ' + res.statusCode);
-        let data = '';
-
-        res.on('data', (d) => {
+          let data = "";
+          
+          res.on('data', (d) => {
             data += d.toString();
+          });
+          
+          res.on('end', () => {
+            let obj = JSON.parse(data);
+            TOKEN = obj["access_token"];
+            resolve(data);  // resolve  
+          });
+
         });
         
-        res.on('end', () => {            
-            callback(data); 
+        req.on('error', reject);
+        
+        req.write(postData);
+        req.end();
+    });
+}
+
+
+function details_code() 
+{
+    return new Promise(function(resolve, reject) {
+
+      var postData = "";
+
+        var options = {
+          hostname: HOST,
+          port: PORT,
+          path: '/examstud/detailscode',
+          method: 'POST',
+          headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': postData.length,
+                'Authorization': "Bearer " + TOKEN
+          }
+        };
+          
+        var req = http.request(options, (res) => {
+          if (res.statusCode != 200)
+              reject('Wrong something.');
+
+          let data = "";
+          
+          res.on('data', (d) => {
+            data += d.toString();
+          });
+          
+          res.on('end', () => {    
+            let model = JSON.parse(data); 
+            if (typeof(model) === "object")               
+                resolve(model);
+            else
+                reject(model);
+          });
+
         });
-
+        
+        req.on('error', reject);
+             
+        req.write(postData);
+        req.end();
     });
-  
-  req.on('error', (e) => {
-    console.error(e);
-  });
-
-  req.write(postData);
-  req.end();
 }
 
+// result of checking: {message, restTime}
 //
-function uppload_code_log(ticket_id, log, callback) 
+function check(examId, userAnswer) 
 {
-    var postData = querystring.stringify({
-        'ticketId' : ticket_id, 
-        'data': JSON.stringify(log),
-        'sender': 'code'         
-    });
-    
-    var options = {
-      hostname: HOST,
-      port: PORT,
-      path: '/examine/codelog',
-      method: 'POST',
-      headers: {
-           'Content-Type': 'application/x-www-form-urlencoded',
-           'Content-Length': postData.length
-         }
-    };
-    
-    var req = http.request(options, (res) => {
-      // vscode.window.showInformationMessage('uppload_code_log: ' + res.statusCode);
-
-      res.on('end', () => {            
-          callback(); 
-      });
-
-    });
-    
-    req.on('error', (e) => {
-      console.error(e);
-    });
-    
-    req.write(postData);
-    req.end();
-}
-
-
-// collback(mes), where mes is result of cheching
-//
-function check(examId, taskId, userAnswer, callback) 
-{
+  return new Promise(function(resolve, reject) {
     var postData = querystring.stringify({
       'examId' : examId, 
-      'taskId' : taskId, 
       'userAnswer': userAnswer, 
     });       
 
@@ -160,34 +124,61 @@ function check(examId, taskId, userAnswer, callback)
     };
 
     var req = http.request(options, (res) => {
-        // vscode.window.showInformationMessage('check: ' + res.statusCode);
-        let data = '';
         
+        let data = '';
         
         res.on('data', (d) => {
             data += d.toString();
         });
         
         res.on('end', () => { 
-            const mes = JSON.parse(data).message;
-            callback(mes);
+            resolve(JSON.parse(data));
         });
-
     });
   
-  req.on('error', (e) => {
-    console.error(e);
-  });
+    req.on('error', reject);
 
-  req.write(postData);
-  req.end();
+    req.write(postData);
+    req.end(); 
+  });
 }
 
-
+//
+function uppload_code_log(ticket_id, log) 
+{
+  return new Promise(function(resolve, reject) {
+    var postData = querystring.stringify({
+      'ticketId' : ticket_id, 
+      'data': JSON.stringify(log),
+      'sender': 'code'         
+    });
+  
+    var options = {
+      hostname: HOST,
+      port: PORT,
+      path: '/examine/codelog',
+      method: 'POST',   
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': postData.length,
+          'Authorization': "Bearer " + TOKEN
+        }
+    };
+  
+    var req = http.request(options, (res) => {
+      res.on('end', resolve);
+    });
+    
+    req.on('error', reject);
+    
+    req.write(postData);
+    req.end();
+  });
+ }
 
 module.exports = {
-    uppload_code_log, 
     token,
-    tss_task,
-    check
+    details_code,
+    check,   
+    uppload_code_log
 }
